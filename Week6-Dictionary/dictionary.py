99c50b5279dafb8bacc9e15849d7b052b8824e14
SABBIR <sabbirto13@gmail.com>
2021-03-09 19:02:58 +0600

    Python for Beginners

--- Week6-Dictionary/dictionary.py
+++ Week6-Dictionary/dictionary.py
@@ -5,11 +5,10 @@ print(city)
 city["Singair"] = 1820
 city["Manikganj"] = 1819
 print(city)
-# Determining if a Dictionary Is Empty or not
 print(len(city))
 
 
-# Another way
+########## Another way ##########
 # Determining if a Dictionary Is Empty or not
 if city:
   print('not empty')
@@ -17,19 +16,18 @@ else:
   print('empty')
   
   
-# Iterate a dictionary
-print(city)
+########## Iterate a dictionary ##########
 for area, code in city.items():
   print(f'{area}’s area code is {code}')
   
   
-# Dictionary operations
+########## Dictionary operations 
 # Accessing a value 
 city["Cantonment"]
-
 # Update a value
 city['Cantonment'] = 1202
 print(city)
+
 
 # updating/insert both key-value respectively
 city.update({'Mohammadpur':1207})
@@ -39,9 +37,11 @@ print(city)
 city.update({'Mohammadpur': 1208})
 print(city)
 
+
 # Adding a new key-value pair
 city['Mirpur'] = 1212
 print(city)
+
 
 # remove a key-value pair
 del city['Cantonment']
--- Week6-Dictionary/set.py
+++ Week6-Dictionary/set.py
@@ -1,22 +1,22 @@
 colors = {'red', 'orange', 'yellow', 'green', 'red', 'blue'}
 print(colors)
 
-
-
-
 # Set methods
 numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-numbers
+print(numbers)
+
 # Add element
 numbers.add(17)
 numbers.add(3)
 # Remove specific element
 numbers.remove(3)
-numbers
+print(numbers)
+
 # Remove first element
 numbers.pop()
-numbers
+print(numbers)
+
 # Empty set 
 numbers.clear()
 # return an empty set
-numbers
+print(numbers)
